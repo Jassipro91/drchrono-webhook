e615045c6dfb619e887fcfcdd4ef314d6bcfc8e3
Jassipro91 <120072543+Jassipro91@users.noreply.github.com>
2026-03-18 16:24:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -32,6 +32,8 @@ app.post('/webhook', async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("Server running");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
